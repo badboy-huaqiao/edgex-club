f55af27a1a679c07da1df062a4eeac8b9b11e827
Huaqiao Zhang <huaqiaoz@vmware.com>
2020-08-01 22:34:02 +0800

    code clean
    Signed-off-by: Huaqiao Zhang <huaqiaoz@vmware.com>

--- internal/core/filter.go
+++ internal/core/filter.go
@@ -16,8 +16,6 @@ func GeneralFilter(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 
-		// log.Printf("path========%v", path)
-
 		if path == "/login.html" || path == "/redirect.html" {
 			http.FileServer(http.Dir("static")).ServeHTTP(w, r)
 			return
@@ -28,22 +26,9 @@ func GeneralFilter(next http.Handler) http.Handler {
 		}
 
 		//Authorization
-
-		// token := r.Header.Get("edgex-club-token")
-		// if token == "" {
-		// 	tokenCookie, err := r.Cookie("edgex-club-token")
-		// 	if err != nil {
-		// 		log.Printf("err========%v", err.Error())
-		// 		http.Error(w, errors.NewErrUnauthorize().Error(), http.StatusUnauthorized)
-		// 		return
-		// 	}
-		// 	token = tokenCookie.Value
-		// }
-
 		var token string
 		if tokenCookie, err := r.Cookie("Authorization"); err == nil {
 			token = tokenCookie.Value
-			// log.Printf("tokenCookie========%v", tokenCookie)
 		}
 
 		var claims *authorization.CustomClaims
@@ -59,7 +44,6 @@ func GeneralFilter(next http.Handler) http.Handler {
 		}
 		if ok {
 			credsByte, err := json.Marshal(claims.Credentials)
-			// log.Printf("credsByte========%v\n", string(credsByte))
 			if err != nil {
 				log.Println("转换creds失败！")
 				http.Error(w, "", http.StatusInternalServerError)
